5ca1e7453737896641f77c1817d8adba5d53bbba
yaohong6782 <yaohong6782@gmail.com>
2023-05-24 02:10:34 +0800

    misc navbar and design changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			robotoslab: ['Roboto Slab'],
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        acme: ['Acme', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 };
